1fec71f94cc30c2ca9495a2be8076dd4f519ed24
djemili <djemili@hotmail.com>
2012-10-25 19:48:04 +0200

    generate script update

--- generate-mendel-dynamics.pl
+++ generate-mendel-dynamics.pl
@@ -13,8 +13,8 @@ use strict;
 
 # Set of plates. Can be done by laser-cut methacrylate, wood, aluminium.
 # Many of them can also be 3D-printed
-my @plates = (); #('triangle', 'base', 'front', 'back', 'bed', 'x-carriage', 'x-end-motor', 'x-end-motor-top', 'x-end-motor-v',
-#	'x-end-idler', 'x-end-idler-top', 'x-end-idler-v', 'y-motor', 'z-motor', 'z-end', 'z-end-bottom');
+my @plates = ('triangle', 'base', 'front', 'back', 'bed', 'x-carriage', 'x-end-motor', 'x-end-motor-top', 'x-end-motor-v',
+	'x-end-idler', 'x-end-idler-top', 'x-end-idler-v', 'y-motor', 'z-motor', 'z-end', 'z-end-bottom');
 
 # Set of printable pieces
 my @pieces = ('bar-clamp', 'belt-clamp', 'corner', 'plate-clamp', 'rod-clamp', 'y-bushing', 'x-bushing-a',
